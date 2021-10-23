4b40b3298c47d04666472215d415eb5c4efde1f1
Ali Hassam <alykhanhssm@gmail.com>
2021-10-22 22:48:03 -0600

    added task model

---
+++ src/app/models/task.model.ts
@@ -0,0 +1,7 @@
+export class Task {
+    id: number | undefined;
+    name: string | undefined;
+    description: string | undefined;
+    estimate: number | undefined;
+    state: string| undefined;
+}
