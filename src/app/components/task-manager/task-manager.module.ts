3f0053b1f650f3a3d60a6afbefc50865597ff180
Ali Hassam <alykhanhssm@gmail.com>
2021-10-24 02:41:59 -0600

    Created task-list and task status components

--- src/app/components/task-manager/task-manager.module.ts
+++ src/app/components/task-manager/task-manager.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TaskManagerRoutingModule } from './task-manager-routing.module';
+import { TaskManagerComponent } from './task-manager.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon'; 
+import { MatButtonModule } from '@angular/material/button';
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskStatusComponent } from './task-status/task-status.component'; 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [TaskManagerComponent, TaskListComponent, TaskStatusComponent],
   imports: [
     CommonModule,
-    TaskManagerRoutingModule
+    TaskManagerRoutingModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule
   ]
 })
 export class TaskManagerModule { }
